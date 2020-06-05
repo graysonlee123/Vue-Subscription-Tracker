55b0ab7bc8a4c81e94d8d4c70a67c2c2fd90c387
Grayson Gantek <graysonleegantek@gmail.com>
2020-06-05 16:26:51 -0400

    dashboard component

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1>Welcome to the dashboard</h1>
+    <h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      msg: 'the commoners'
+    }
+  }
+}
+</script>
+
+<style scoped>
+    h1, h2 {
+        font-weight: normal;
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+    a {
+        color: #42b983;
+    }
+</style>
