e23510f98071ebb71acf805cb5c2f12cb5845b49
Grayson Gantek <graysonleegantek@gmail.com>
2020-06-05 16:16:51 -0400

    adds login component

---
+++ src/components/Login.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h4>Login</h4>
+    <form>
+      <label for="email">Email</label>
+      <div>
+        <input id="email" type="text" v-model="email" autofocus />
+      </div>
+      <label for="password">Password</label>
+      <div>
+        <input id="password" type="text" v-model="email" autofocus />
+      </div>
+      <button type="submit" @click="handleSubmit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+
+      if (this.password.length > 0) {
+        this.$http
+          .post("http://localhost:3000/login", {
+            email: this.email,
+            password: this.password
+          })
+          .then(res => {
+            localStorage.setItem('user', JSON.stringify(response.data.user));
+            localStorage.setItem('jwt', response.data.token);
+
+            if (localStorage.getItem('jwt') != null) {
+              this.$emit('loggedIn');
+
+              if(this.$route.params.nextUrl != null) {
+                this.$router.push(this.$route.params.nextUrl)
+              } else {
+                this.$router.push('dashboard');
+              }
+            }
+          })
+          .catch(err => {
+            console.error(error.response);
+          });
+      }
+    }
+  }
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,82 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import Login from "@/components/Login";
+import Register from "@/components/Register";
+import Dashboard from "@/components/Dashboard";
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
+let router = new Router({
+  mode: "history",
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
+      path: "/",
+      name: "HelloWorld",
       component: HelloWorld
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
+      meta: {
+        guest: true
+      }
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: Register,
+      meta: {
+        guest: true
+      }
+    },
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      component: Dashboard,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
-})
+});
+
+// Vue has this method which is called before each route is processed,
+// so this is where we define our checking conditions and restrict access
+
+// @param to    where the user wishes to go
+// @param from  where the user is coming from
+// @param next  a callback function that continues the process
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (localStorage.getItem("jwt") == null) {
+      next({
+        path: "/login",
+        params: { nextUrl: to.fullPath }
+      });
+    } else {
+      let user = JSON.parse(localStorage.getItem("user"));
+
+      if (to.matched.some(record => record.meta.is_admin)) {
+        if (user.is_admin == 1) {
+          next();
+        } else {
+          next({ name: "dashboard" });
+        }
+      } else {
+        next();
+      }
+    }
+  } else if (to.matched.some(record => record.meta.guest)) {
+    if (localStorage.getItem("jwt") == null) {
+      next();
+    } else {
+      next({ name: "dashboard" });
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
