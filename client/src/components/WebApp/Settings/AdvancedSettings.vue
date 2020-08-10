28bb23cc1a1e724b2d94b41fcebd01673aa1d863
Grayson Gantek <graysonleegantek@gmail.com>
2020-08-09 21:59:33 -0400

    starts adding account settings - adds preferences, account, password, and advanced tab with corresponding routes

---
+++ client/src/components/WebApp/Settings/AdvancedSettings.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="advanced__wrapper">
+    <input type="button" value="Delete Account" class="roundedButton danger" @click="deleteAccount()"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {};
+  },
+  methods: {
+    deleteAccount: function () {
+      if (
+        confirm(
+          "Are you sure you want to delete your account? This action cannot be undone. All data associated with the account will be erased."
+        )
+      ) {
+        console.log('Delete Account');
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.advanced__wrapper {
+  padding: 2em 0;
+}
+</style>
